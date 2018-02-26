75af14c2ee1f4195f6da78c4f6d40bfbbd778b08
Fabian Bühler <st116290@stud.uni-stuttgart.de>
2018-02-26 15:22:50 +0100

    added all taxonomies in backend

--- app/sqlClient/taxonomies.js
+++ app/sqlClient/taxonomies.js
@@ -6,19 +6,36 @@ var resultUtils = require('./dbResultUtils');
 var genericUtils = require('./dbGenericUtils');
 
 var taxonomies = {
-    //'': { type: 'list', tableName: '', columnName: '' },
-    //'t': { type: 'tree', tableName: '', columnName: '', parentColumnName: '' },
-    'Basiselement': { type: 'tree', tableName: 'BasiselementDomaene', columnName: 'BasiselementName', parentColumnName: 'UebergeordnetesElement' },
-    'Genre': { type: 'tree', tableName: 'GenreDomaene', columnName: 'GenreName', parentColumnName: 'UebergeordnetesElement' },
-    'Körpermodifikationen': { type: 'tree', tableName: 'KoerpermodifikationsDomaene', columnName: 'KoerpermodifikationsName', parentColumnName: 'UebergeordnetesElement' },
-    'Material': { type: 'tree', tableName: 'MaterialDomaene', columnName: 'MaterialName', parentColumnName: 'UebergeordnetesElement' },
-    'Produktionsort': { type: 'tree', tableName: 'ProduktionsortDomaene', columnName: 'ProduktionsortName', parentColumnName: 'UebergeordnetesElement' },
-    'Beruf': { type: 'tree', tableName: 'RollenberufDomaene', columnName: 'RollenberufName', parentColumnName: 'UebergeordnetesElement' },
-    'Spielort': { type: 'tree', tableName: 'SpielortDomaene', columnName: 'SpielortName', parentColumnName: 'UebergeordnetesElement' },
-    'Spielort (Detail)': { type: 'tree', tableName: 'SpielortDetailDomaene', columnName: 'SpielortDetailName', parentColumnName: 'UebergeordnetesElement' },
-    'Spielzeit': { type: 'tree', tableName: 'SpielzeitDomaene', columnName: 'SpielzeitName', parentColumnName: 'UebergeordnetesElement' },
-    'Stereotyp': { type: 'tree', tableName: 'StereotypDomaene', columnName: 'StereotypName', parentColumnName: 'UebergeordnetesElement' },
-    'Teilelement': { type: 'tree', tableName: 'TeilelementDomaene', columnName: 'TeilelementName', parentColumnName: 'UebergeordnetesElement' },
+    //'': { type: 'list', editable: false, tableName: '', columnName: '' },
+    //'t': { type: 'tree', editable: false, tableName: '', columnName: '', parentColumnName: '' },
+    'Basiselement': { type: 'tree', editable: true, tableName: 'BasiselementDomaene', columnName: 'BasiselementName', parentColumnName: 'UebergeordnetesElement' },
+    'Genre': { type: 'tree', editable: true, tableName: 'GenreDomaene', columnName: 'GenreName', parentColumnName: 'UebergeordnetesElement' },
+    'Körpermodifikationen': { type: 'tree', updataeditableble: true, tableName: 'KoerpermodifikationsDomaene', columnName: 'KoerpermodifikationsName', parentColumnName: 'UebergeordnetesElement' },
+    'Material': { type: 'tree', editable: true, tableName: 'MaterialDomaene', columnName: 'MaterialName', parentColumnName: 'UebergeordnetesElement' },
+    'Produktionsort': { type: 'tree', editable: true, tableName: 'ProduktionsortDomaene', columnName: 'ProduktionsortName', parentColumnName: 'UebergeordnetesElement' },
+    'Beruf': { type: 'tree', editable: true, tableName: 'RollenberufDomaene', columnName: 'RollenberufName', parentColumnName: 'UebergeordnetesElement' },
+    'Spielort': { type: 'tree', editable: true, tableName: 'SpielortDomaene', columnName: 'SpielortName', parentColumnName: 'UebergeordnetesElement' },
+    'Spielort (Detail)': { type: 'tree', editable: true, tableName: 'SpielortDetailDomaene', columnName: 'SpielortDetailName', parentColumnName: 'UebergeordnetesElement' },
+    'Spielzeit': { type: 'tree', editable: true, tableName: 'SpielzeitDomaene', columnName: 'SpielzeitName', parentColumnName: 'UebergeordnetesElement' },
+    'Stereotyp': { type: 'tree', editable: true, tableName: 'StereotypDomaene', columnName: 'StereotypName', parentColumnName: 'UebergeordnetesElement' },
+    'Teilelement': { type: 'tree', editable: true, tableName: 'TeilelementDomaene', columnName: 'TeilelementName', parentColumnName: 'UebergeordnetesElement' },
+
+    'Charaktereigenschaft': { type: 'tree', editable: false, tableName: 'CharaktereigenschaftsDomaene', columnName: 'CharaktereigenschaftsName', parentColumnName: 'UebergeordnetesElement' },
+    'Tageszeit':  { type: 'tree', editable: false, tableName: 'TageszeitDomaene', columnName: 'TageszeitName', parentColumnName: 'UebergeordnetesElement' },
+    'Alterseindruck': { type: 'tree', editable: false, tableName: 'AlterseindruckDomaene', columnName: 'AlterseindruckName', parentColumnName: 'UebergeordnetesElement' },
+    'Zustand': { type: 'tree', editable: false, tableName: 'ZustandsDomaene', columnName: 'ZustandsName', parentColumnName: 'UebergeordnetesElement' },
+    'Farbe': { type: 'tree', editable: false, tableName: 'FarbenDomaene', columnName: 'FarbName', parentColumnName: 'UebergeordnetesElement' },
+    'Farbkonzept': { type: 'tree', editable: false, tableName: 'FarbkonzeptDomaene', columnName: 'FarbkonzeptName', parentColumnName: 'UebergeordnetesElement' },
+    'Design': { type: 'tree', editable: false, tableName: 'DesignDomaene', columnName: 'DesignName', parentColumnName: 'UebergeordnetesElement' },
+    'Form': { type: 'tree', editable: false, tableName: 'FormenDomaene', columnName: 'FormName', parentColumnName: 'UebergeordnetesElement' },
+    'Trageweise': { type: 'tree', editable: false, tableName: 'TrageweisenDomaene', columnName: 'TrageweisenName', parentColumnName: 'UebergeordnetesElement' },
+    'Funktion': { type: 'tree', editable: false, tableName: 'FunktionsDomaene', columnName: 'FunktionsName', parentColumnName: 'UebergeordnetesElement' },
+    'Operator': { type: 'tree', editable: false, tableName: 'OperatorDomaene', columnName: 'OperatorName', parentColumnName: 'UebergeordnetesElement' },
+    'Körperteil': { type: 'tree', editable: false, tableName: 'KoerperteilDomaene', columnName: 'Koerperteilname', parentColumnName: 'UebergeordnetesElement' },
+    'Dominante Charaktereigenschaft': { type: 'tree', editable: false, tableName: 'TypusDomaene', columnName: 'TypusName', parentColumnName: 'UebergeordnetesElement' },
+
+    'Materialeindruck': { type: 'list', tableName: 'MaterialeindruckDomaene', columnName: 'Materialeindruckname' },
+    'Farbeindruck': { type: 'list', editable: false, tableName: 'FarbeindruckDomaene', columnName: 'Farbeindruckname' },
 };
 
 function updateableTaxonomies() {
@@ -27,7 +44,7 @@ function updateableTaxonomies() {
     var updateableTaxonomies = [];
 
     Object.keys(taxonomies).forEach(function (name) {
-        updateableTaxonomies.push({ name: name, type: taxonomies[name].type });
+        updateableTaxonomies.push({ name: name, type: taxonomies[name].type, editable: taxonomies[name].editable });
     });
 
     deferred.resolve(updateableTaxonomies);
@@ -42,7 +59,12 @@ function deleteTaxonomyElement(taxonomy, name) {
     var taxonomyInfo = taxonomies[taxonomy];
 
     if (taxonomyInfo) {
-        return genericUtils.deleteElementMappings(taxonomyInfo.tableName, taxonomyInfo.columnName, name);
+        if (taxonomyInfo.editable) {
+            return genericUtils.deleteElementMappings(taxonomyInfo.tableName, taxonomyInfo.columnName, name);
+        } else {
+            deferred.reject('Taxonomy ' + taxonomy + ' is not editable!');
+            return deferred.promise;
+        }
     } else {
         deferred.reject('Unknown taxonomy ' + taxonomy);
         return deferred.promise;
@@ -56,16 +78,21 @@ function addTaxonomyElement(taxonomy, element) {
     var taxonomyInfo = taxonomies[taxonomy];
 
     if (taxonomyInfo) {
-        var mapping = {};
-        if (taxonomyInfo.type === 'list') {
-            mapping[taxonomyInfo.columnName] = element.name;
+        if (taxonomyInfo.editable) {
+            var mapping = {};
+            if (taxonomyInfo.type === 'list') {
+                mapping[taxonomyInfo.columnName] = element.name;
 
-            return genericUtils.createElementTripleMapping(taxonomyInfo.tableName, mapping);
+                return genericUtils.createElementTripleMapping(taxonomyInfo.tableName, mapping);
+            } else {
+                mapping[taxonomyInfo.columnName] = element.name;
+                mapping[taxonomyInfo.parentColumnName] = element.parent;
+
+                return genericUtils.createElementTripleMapping(taxonomyInfo.tableName, mapping);
+            }
         } else {
-            mapping[taxonomyInfo.columnName] = element.name;
-            mapping[taxonomyInfo.parentColumnName] = element.parent;
-
-            return genericUtils.createElementTripleMapping(taxonomyInfo.tableName, mapping);
+            deferred.reject('Taxonomy ' + taxonomy + ' is not editable!');
+            return deferred.promise;
         }
     } else {
         deferred.reject('Unknown taxonomy ' + taxonomy);
